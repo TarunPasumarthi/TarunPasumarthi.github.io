364befd1a35875abe49c4c4d4368157897dfb110
Tarun Pasumarthi <tyco96@gmail.com>
2018-04-17 12:21:17 -0700

    added files

---
+++ js/js1.js
@@ -0,0 +1,21 @@
+function skills(){
+	window.location.href="./Skills.html";
+}
+function projects(){
+	window.location.href="./Projects.html";
+}
+function sudoku(){
+	window.location="https://github.com/TarunPasumarthi/Sudoku";
+}
+function app(){
+	window.location="https://github.com/chris1625/StudyBananas";
+}
+function ultimo(){
+	window.location="http://www.ultimosoft.com/errorspot/errorspot.html";
+}
+function prch(){
+	window.location="http://www.theprch.com/";
+}
+function resume(){
+	window.location.href="./Tarun_Pasumarthi_Resume.pdf";
+}
